2a79a9627225fd3b095709195b3fba95ea44e32b
entiflags <104980440+entiflags@users.noreply.github.com>
2025-05-09 19:59:39 +0600

    porting code from https://wiki.osdev.org/Bare_Bones
    thank to ai for help with error fixing =)

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
 #define vgaw 80
 #define vgah 25 
 #define vidPtr (char*)0xb8000 //video memory starting here
@@ -6,25 +10,97 @@
 void clearScr(char *vidptr);
 void printString(char *vidptr, const char *str, unsigned int *i);
 
+enum vgacols{
+	black=0,
+	blue=1,
+	green=2,
+	cyan=3,
+	red=4,
+	magenta=5,
+	brown=6,
+	lightgrey=7,
+	darkgrey=8,
+	lightblue=9,
+	lightgreen=10,
+	lightcyan=11,
+	lightred=12,
+	lightmagenta=13,
+	lightbrown=14,
+	white=15,
+};
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer = (uint16_t*)vidPtr;
+
+static inline uint8_t vgaEntryCol(enum vgacols fg, enum vgacols bg){
+	return fg | bg << 4;
+}
+
+static inline uint16_t vgaEntry(unsigned char uc,uint8_t color){
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+size_t strlen(const char* str){
+	size_t len=0;
+	while(str[len])
+		len++;
+	return len;
+}
+
+void termInit(void){
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vgaEntryCol(lightcyan,black);
+	
+	for(size_t y=0;y<vgah;y++){
+		for(size_t x=0;x<vgaw;x++){
+			const size_t index = y * vgaw + x;
+			terminal_buffer[index] = vgaEntry(' ', terminal_color);
+		}
+	}
+}
+
+void termSetCol(uint8_t color)
+{
+	terminal_color = color;
+}
+
+void termPutEntryAt(char c,uint8_t color,size_t x,size_t y){
+	const size_t index = y * vgaw + x;
+	terminal_buffer[index] = vgaEntry(c,color);
+}
+
+void termPutC(char c){
+	termPutEntryAt(c,terminal_color,terminal_column,terminal_row);
+	if(++terminal_column == vgaw){
+		terminal_column = 0;
+		if(++terminal_row == vgah)
+			terminal_row = 0;
+	}	
+}
+
+void termWrite(const char* data,size_t size){
+	for(size_t i=0;i<size;i++)
+		termPutC(data[i]);
+}	
+
 //function called in bootloader
 void kmain(void)
 {
 	char *vidptr = vidPtr;
 	unsigned int i=0;
+	termInit();
 
 	clearScr(vidptr);	//clearing screen
-	printString(vidptr,"kakashka",&i);	//printing 'kakashka' to screen
-	return;	//exit the function
+	printString(vidptr,"kakashka\n",&i);	//printing 'kakashka' to screen
 }
 
 void printString(char *vidptr, const char *str, unsigned int *i){
-	unsigned int j = 0;
-	while(str[j] != '\0' && *i < vgasz){//putting text to video mem until string end
-		vidptr[*i] = str[j];	//writing current char to video mem
-		vidptr[*i + 1] = 0x0b;	//set attribute byte
-		++j;	//incrementing string index to move next character
-		*i += 2;	//incrementing video mem index by 2
-	}
+	(void)vidptr;
+	(void)i;
+	termWrite(str, strlen(str));
 }
 
 void clearScr(char *vidptr){
